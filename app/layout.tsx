394aab0e37f90e574c0a08349b3547b22387c52b
Joshua-Vallabhaneni <pjvallabhaneni@gmail.com>
2025-02-20 12:09:52 -0500

    added analytics feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <ModeToggle />
           </div>
           {children}
+          <Analytics /> {/* Place Analytics here */}
         </ThemeProvider>
       </body>
     </html>
